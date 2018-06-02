5c15c08d0a5798e16920ce713cf738a73167f29c
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-06-02 18:32:18 +0700

    Home Activity - Open Add Things activity through Add to List button

--- app/src/main/java/id/co/fatimazza/notethings/home/HomeActivity.java
+++ app/src/main/java/id/co/fatimazza/notethings/home/HomeActivity.java
@@ -1,12 +1,15 @@
 package id.co.fatimazza.notethings.home;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.TextView;
 
 import id.co.fatimazza.notethings.R;
+import id.co.fatimazza.notethings.addthings.AddThingsActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -25,5 +28,9 @@ public class HomeActivity extends AppCompatActivity {
         rvListOfThings = findViewById(R.id.rv_listof_things);
         tvEmpty = findViewById(R.id.tv_empty_data);
         fabAdd = findViewById(R.id.fab_add);
+    }
+
+    public void addNewThingToList (View view) {
+        startActivity(new Intent(this, AddThingsActivity.class));
     }
 }
