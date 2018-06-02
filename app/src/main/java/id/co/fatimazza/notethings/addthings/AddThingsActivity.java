1920a344a822acd52a462eee94d4383c4e311851
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-06-02 18:21:27 +0700

    Add Things Activity - Add activity to add detailed thing, Add layout and array values

---
+++ app/src/main/java/id/co/fatimazza/notethings/addthings/AddThingsActivity.java
@@ -0,0 +1,15 @@
+package id.co.fatimazza.notethings.addthings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.co.fatimazza.notethings.R;
+
+public class AddThingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_things);
+    }
+}
